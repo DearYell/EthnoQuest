d9e2e8e8e590d639cc753f9e7c039b393f973e04
Mark Edwin Huyo-a <markhuyoa2015@gmail.com>
2023-11-22 17:06:04 +0800

    Modify dashboard.js file a little

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,146 +1,44 @@
-import * as React from "react";
+import React, { useState } from "react";
 import {
   Button,
   Typography,
-  // Card,
-  // CardContent,
   List,
-  // ListItem,
-  ListItemText as MuiListItemText, // Rename to avoid conflict
-} from "@material-ui/core"; // Import all components from material-ui/core
+  ListItemText as MuiListItemText,
+} from "@material-ui/core";
 import { styled, createTheme, ThemeProvider } from "@mui/material/styles";
-// import Divider from "@mui/material/Divider"; // Import the Divider component
 import CssBaseline from "@mui/material/CssBaseline";
 import MuiDrawer from "@mui/material/Drawer";
 import Box from "@mui/material/Box";
 import MuiAppBar from "@mui/material/AppBar";
-import LocationOnIcon from '@mui/icons-material/LocationOn';
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import { Link } from "react-router-dom";
 import MenuIcon from "@mui/icons-material/Menu";
-// import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import SearchIcon from "@mui/icons-material/Search";
 import TextField from "@mui/material/TextField";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
-// import ListItemText from "@mui/material/ListItemText";
 import DashboardIcon from "@mui/icons-material/Dashboard";
-<<<<<<< HEAD
 import CompassCalibrationIcon from "@mui/icons-material/CompassCalibration";
 import SettingsIcon from "@mui/icons-material/Settings";
 import LogoutIcon from "@mui/icons-material/Logout";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import "./index.css";
 // import { lightGreen } from "@mui/material/colors";
-=======
-import SettingsIcon from '@mui/icons-material/Settings';
-import LogoutIcon from '@mui/icons-material/Logout';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-
-
-const LogoListItem = (
-  <ListItemButton>
-    <ListItemIcon>
-      <img src="./Logo.png" alt="Logo" style={{ maxHeight: "40px" }} />
-    </ListItemIcon>
-    <MuiListItemText primary="EthnoQuest" />
-  </ListItemButton>
-);
-
-export const mainListItems = (
-  <React.Fragment>
-    {LogoListItem}
-    <ListItemButton>
-      <ListItemIcon>
-        <DashboardIcon style={{ color: 'lightgreen' }}/>
-      </ListItemIcon >
-      <ListItemText primary="Dashboard" />
-    </ListItemButton>
-    <ListItemButton component={Link} to="/AllCapitals">
-      <ListItemIcon>
-        <LocationOnIcon />
-      </ListItemIcon>
-      <ListItemText primary="All Capitals" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <AccountCircleIcon />
-      </ListItemIcon>
-      <ListItemText primary="My Profiles" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <SettingsIcon />
-      </ListItemIcon>
-      <ListItemText primary="Settings" />
-    </ListItemButton>
-    <ListItemButton component={Link} to="/Login">
-      <ListItemIcon>
-        <LogoutIcon />
-      </ListItemIcon>
-      <ListItemText primary="Log Out" />
-    </ListItemButton>
-  </React.Fragment>
-);
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== "open",
-})(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(["width", "margin"], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    marginLeft: 240,
-    width: `calc(100% - 240px)`,
-    transition: theme.transitions.create(["width", "margin"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
-
-const Drawer = styled(MuiDrawer, {
-  shouldForwardProp: (prop) => prop !== "open",
-})(({ theme, open }) => ({
-  "& .MuiDrawer-paper": {
-    position: "relative",
-    whiteSpace: "nowrap",
-    width: 240,
-    transition: theme.transitions.create("width", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    boxSizing: "border-box",
-    ...(!open && {
-      overflowX: "hidden",
-      transition: theme.transitions.create("width", {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-      }),
-      width: theme.spacing(7),
-      [theme.breakpoints.up("sm")]: {
-        width: theme.spacing(9),
-      },
-    }),
-  },
-}));
 
 const defaultTheme = createTheme();
->>>>>>> 782945920b87b3b8e80ea716bc84cec0b889639d
 
 export default function Dashboard() {
-  const [open, setOpen] = React.useState(true);
+  const [open, setOpen] = useState(true);
+  const [rotation, setRotation] = useState(0);
+  const [activeButton, setActiveButton] = useState(null);
+
   const toggleDrawer = () => {
     setOpen(!open);
   };
-
-<<<<<<< HEAD
-  const defaultTheme = createTheme();
 
   React.useEffect(() => {
     const rotateInterval = setInterval(() => {
@@ -277,10 +175,7 @@ export default function Dashboard() {
     },
   }));
 
-=======
->>>>>>> 782945920b87b3b8e80ea716bc84cec0b889639d
   return (
-    
     <ThemeProvider theme={defaultTheme}>
       <Box sx={{ display: "flex" }}>
         <CssBaseline />
@@ -303,7 +198,7 @@ export default function Dashboard() {
             >
               <MenuIcon />
             </IconButton>
-            
+
             <Typography
               component="div"
               variant="h6"
@@ -316,6 +211,7 @@ export default function Dashboard() {
                 marginLeft: "8px",
               }}
             >
+              <SearchIcon style={{ marginTop: "10px", marginLeft: "100px" }} />
               <TextField
                 label="Search capital around the world"
                 variant="outlined"
@@ -342,8 +238,10 @@ export default function Dashboard() {
               <ChevronLeftIcon />
             </IconButton>
           </Toolbar>
-          <Divider />
-          <List component="nav">{mainListItems}</List>
+          <List component="nav">
+            {LogoListItem}
+            {mainListItems}
+          </List>
         </Drawer>
         <Box
           component="main"
@@ -363,7 +261,8 @@ export default function Dashboard() {
               left: 0,
               right: 0,
               bottom: 0,
-              backgroundImage: "linear-gradient(180deg, rgba(49, 210, 55, 0.47) 24.13%, rgba(6, 222, 196, 0.54) 74.13%)",
+              backgroundImage:
+                "linear-gradient(180deg, rgba(49, 210, 55, 0.47) 24.13%, rgba(6, 222, 196, 0.54) 74.13%)",
               backdropFilter: "blur(4px)",
               overflow: "hidden",
               backgroundSize: "cover",
@@ -374,13 +273,9 @@ export default function Dashboard() {
             </Grid>
             <Copyright sx={{ pt: 4 }} />
           </Container>
-
-
-
         </Box>
       </Box>
     </ThemeProvider>
-    
   );
 }
 
@@ -390,27 +285,26 @@ function ProfileCircle() {
   return (
     <div
       style={{
-        position: 'fixed',
-        top: '10px',
-        left: 'calc(100% - 90px)', /* Adjusted value */
-        display: 'flex',
-        alignItems: 'center',
+        position: "fixed",
+        top: "10px",
+        left: "calc(100% - 90px)" /* Adjusted value */,
+        display: "flex",
+        alignItems: "center",
       }}
     >
       <img
         src={profileImgUrl}
         alt=""
-        style={{ width: '40px', height: '40px', borderRadius: '50%', objectFit: 'cover' }}
+        style={{
+          width: "40px",
+          height: "40px",
+          borderRadius: "50%",
+          objectFit: "cover",
+        }}
       />
     </div>
   );
 }
-
-
-
-
-
-
 
 function Copyright(props) {
   return (
