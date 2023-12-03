eed76b49558d8bfc8c04f90f31ca4b25b19592e1
janickaalgonas <janickamarie.algonas@cit.edugit config --global user.name>
2023-12-03 12:46:38 +0800

    Routes

--- src/App.js
+++ src/App.js
@@ -10,28 +10,24 @@ import MHoliday from "./MHoliday";
 import MTradition from "./MTradition";
 import MyProfile from "./MyProfile";
 import QuizHistory from "./QuizHistory";
-//import Settings from "./Settings";
 import AdminDashboard from "./dashboardAdmin";
-<<<<<<< HEAD
+import ContactUs from "./ContactUs";
 
 //import Quiz from "./Quiz"
-// import AboutUs from "./AboutUs";
-=======
 import AboutUs from "./AboutUs";
-import ContactUs from "./ContactUs";
->>>>>>> 90deaf3ddcd758016d1ca83c75893311e79fc3b2
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route index element={<SignInSide />} />
-          <Route path="/Login" element={<SignInSide />} />
-          <Route path="/Register/*" element={<RegistrationForm />} />
-          <Route path="/AdminVer" element={<AdminVerification />} />
+        <Route path="/AdminVer" element={<AdminVerification />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/AllCapitals" element={<Capitals />} />
+          <Route path="/MHistory" element={<MHistory />} />
+          <Route path="/MCulture" element={<MCulture />} />
+          <Route path="/MTradition" element={<MTradition />} />
+          <Route path="/MHoliday" element={<MHoliday />} />
           <Route path="/MHistory/:id" element={<MHistory />} />
           <Route path="/MCulture/:id" element={<MCulture />} />
           <Route path="/MTradition/:id" element={<MTradition />} />
@@ -40,12 +36,8 @@ function App() {
           <Route path="/QuizHistory" element={<QuizHistory />} />
           {/* <Route path="/Settings" element={<Settings />} /> */}
           <Route path="/dashboardAdmin" element={<AdminDashboard />} />
-<<<<<<< HEAD
-          <Route path="/QHistory" element={<quizHistory />} />
-=======
           <Route path="/AboutUs" element={<AboutUs />} />
           <Route path="/ContactUs" element={<ContactUs />} />
->>>>>>> 90deaf3ddcd758016d1ca83c75893311e79fc3b2
         </Routes>
       </BrowserRouter>
     </div>
